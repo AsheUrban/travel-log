27eb263d0312ea93198913570d33e0a3a0f40ce7
Ashe Urban <ashe@goldentongue.com>
2023-01-23 14:37:44 -0800

    Complete business logic and WIP ui logic.
    Co-authored-by: Jacob Maier<jacobamaier@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -5,12 +5,12 @@ function TravelLog() {
   this.currentId = 0;
 }
 
-TravelLog.prototype.addPlace = function(place) {
+TravelLog.prototype.addPlace = function (place) {
   place.id = this.assignId();
   this.places[place.id] = place;
 };
 
-TravelLog.prototype.assignId = function() {
+TravelLog.prototype.assignId = function () {
   this.currentId += 1;
   return this.currentId;
 };
@@ -23,6 +23,20 @@ function Place(placeName, location, note) {
   this.note = note;
 }
 
+// UI LOGIC -------
 
+let travelLog = new TravelLog();
 
+function handleFormSubmission(event) {
+  event.preventDefault();
+  const inputtedPlaceName = document.querySelector("input#new-place-name").value;
+  const inputtedLocation = document.querySelector("input#new-location").value;
+  const inputtedNote = document.querySelector("input#new-note").value;
+  let newPlace = new Place(inputtedPlaceName, inputtedLocation, inputtedNote);
+  travelLog.addPlace(newPlace);
+  console.log(travelLog.places);
+}
 
+window.addEventListener("load", function () {
+  this.document.querySelector("form#new-place").addEventListener("submit", handleFormSubmission);
+});
